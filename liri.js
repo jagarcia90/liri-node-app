d84ac31dced62059e74c4cfa221c8a222ced5b77
Jorge Garcia <jagarcia@ucdavis.edu>
2018-08-04 13:06:28 -0700

    update

--- liri.js
+++ liri.js
@@ -55,15 +55,9 @@ switch (command) {
     //-->go sign up for spotify accoutns
  };
  function omdb(movie_name){
-    
     var request = require("request");
-
-// Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-  // If the request is successful (i.e. if the response status code is 200)
+    request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
   if (!error && response.statusCode === 200) {
-
     // Parse the body of the site and recover just the imdbRating
     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
@@ -84,7 +78,6 @@ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tril
       * Plot of the movie.
       * Actors in the movie.
     ```
-  * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
  */
 };
 
